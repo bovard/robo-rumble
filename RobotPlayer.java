a1d155be37cd4053c9cd0e99c19b07fa2148a09d
Bovard Tiberi <bovard.tiberi@gmail.com>
2011-01-13 21:21:35 +0000

    added the staging function to select which system to load on a given bot

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -25,12 +25,53 @@ public class RobotPlayer implements Runnable {
       runMotor((MovementController)components[0]);
   }
 
-  public void testit(MovementController m) {
-    m.withinRange(myRC.getLocation());
+  /**
+   * staging will house a robot until it has enough components to be a recognized system
+   * after that it will pass the robotControl to the system and call the go() method
+   */
+  public void staging() {
+    ComponentController [] components;
+    //one the code enters here it waits until it recognizes one of the builds, then loads
+    //the appropriate system
+    while(true) {
+      components = myRC.components();
+
+      if (myRC.getChassis() == Chassis.LIGHT) {
+        //our BUILDER_SCOUT
+        if (components.length == 3 && components[1].type()==ComponentType.SIGHT &&
+                components[2].type()==ComponentType.CONSTRUCTOR) {
+          BuilderScoutRobotSystem system = new BuilderScoutRobotSystem(myRC);
+          system.go();
+        }
+        //provided BuilderScout
+        else if (components.length == 3 && components[2].type()==ComponentType.SIGHT &&
+                components[1].type()==ComponentType.CONSTRUCTOR) {
+          BuilderScoutRobotSystem system = new BuilderScoutRobotSystem(myRC);
+          system.go();
+        }
+        //FIGHTER_SCOUT
+        else if (components.length == 4 && components[1].type()==ComponentType.RADAR &&
+                components[1].type()==ComponentType.SMG && components[2].type()==ComponentType.SHIELD) {
+          FighterScoutRobotSystem system = new FighterScoutRobotSystem(myRC);
+          system.go();
+        }
+      }
+      else if (myRC.getChassis()==Chassis.BUILDING) {
+        //RECYCLER
+        if (components.length == 3 && components[1].type() == ComponentType.BUILDING_SENSOR &&
+                components[2].type()==ComponentType.RECYCLER) {
+          RecyclerRobotSystem system = new RecyclerRobotSystem(myRC);
+          system.go();
+        }
+      }
+      //we didn't find our system, yield for a turn and try again next time
+      myRC.yield();
+    }
   }
 
   //this is a dirty hack to make light chassis bots with 3 SMGs and a radar
   public void runBuilder(MovementController motor, BuilderController builder) {
+
     while (true) {
       try {
         //yield at the top of every loop
