42c1ad716fe6c67eeba7769b572eb7eb513ceb19
Bovard Tiberi <bovard.tiberi@gmail.com>
2011-01-14 13:30:51 +0000

    cleaed up RobotPlayer, no more dirty hax :(

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -8,21 +8,13 @@ public class RobotPlayer implements Runnable {
   private final RobotController myRC;
 
   public RobotPlayer(RobotController rc) {
-    System.out.println("System Constant: "+GameConstants.REGEN_AMOUNT);
     myRC = rc;
   }
 
   public void run() {
-    //this wait may be necessary? all part of the dirty hack
-    for (int i=0; i<6; i++)
-      myRC.yield();
-    ComponentController [] components = myRC.newComponents();
-    System.out.println(java.util.Arrays.toString(components));
+    System.out.println(java.util.Arrays.toString(myRC.components()));
     System.out.flush();
-    if(myRC.getChassis()==Chassis.BUILDING)
-      runBuilder((MovementController)components[0],(BuilderController)components[2]);
-    else
-      runMotor((MovementController)components[0]);
+    staging();
   }
 
   /**
@@ -53,7 +45,7 @@ public class RobotPlayer implements Runnable {
         }
         //FIGHTER_SCOUT
         else if (components.length == 4 && components[1].type()==ComponentType.RADAR &&
-                components[1].type()==ComponentType.SMG && components[2].type()==ComponentType.SHIELD) {
+                components[1].type()==ComponentType.BLASTER && components[2].type()==ComponentType.SHIELD) {
           FighterScoutRobotSystem system = new FighterScoutRobotSystem(myRC);
           system.go();
         }
@@ -68,64 +60,6 @@ public class RobotPlayer implements Runnable {
       }
       //we didn't find our system, yield for a turn and try again next time
       myRC.yield();
-    }
-  }
-
-  //this is a dirty hack to make light chassis bots with 3 SMGs and a radar
-  public void runBuilder(MovementController motor, BuilderController builder) {
-    RecyclerRobotSystem system = new RecyclerRobotSystem(myRC);
-    system.go();
-  }
-
-  //This is a dirty hack for testing, looks for a light chassis with 1+ SMGs and a radar
-  public void runMotor(MovementController motor) {
-    SensorSystem sensorSys = null;
-    WeaponSystem weaponSys = null;
-    NavigationSystem nav = new NavigationSystem(motor);
-    
-    //try to see if we can pull out a radar from the components and get a sensorSys running on it
-    //also count # weapons
-    int weaponCount = 0;
-    for (int i=0;i<myRC.components().length;i++) {
-      if (myRC.components()[i].type() == ComponentType.RADAR)
-        sensorSys = new SensorSystem((SensorController)myRC.components()[i]);
-      else if(myRC.components()[i].type() == ComponentType.SMG)
-        weaponCount++;
-    }
-
-    //if we have SMGs and a radar, make a weaponSys to fire them!
-    if (weaponCount > 0 && sensorSys != null) {
-      WeaponController[] weapons = new WeaponController[weaponCount];
-      weaponCount = 0;
-      for (int i=0;i<myRC.components().length;i++) {
-        if (myRC.components()[i].type() == ComponentType.SMG) {
-          weapons[weaponCount] = (WeaponController)myRC.components()[i];
-          weaponCount++;
-        }
-      }
-      weaponSys = new WeaponSystem(weapons, sensorSys);
-    }
-
-    //all new bots will move to this new location... choose it well? if it's off the map it doesn't matter
-    //chain use North +30 West +15 FTW
-    //quadrents use North +52 West +52
-    //hooks use South +30 West +1
-    MapLocation dest = myRC.getLocation().add(Direction.SOUTH, 30).add(Direction.WEST, 1);
-    nav.setDestination(dest);
-    myRC.yield();
-    int i = 0;
-    while (true) {
-      try {
-        while(!nav.nextMove()) {
-          if(weaponSys!=null)
-            weaponSys.fire();
-          myRC.yield();
-        }
-      }
-      catch (Exception e) {
-        System.out.println("caught exception:");
-        e.printStackTrace();
-      }
     }
   }
 }
