4cc2cad5b211dedbce2fd8c583b308ccdfc87bb0
Bovard Tiberi <bovard.tiberi@gmail.com>
2011-01-23 00:11:33 +0000

    changed teh nav system methods around to actually make sense

--- NavigationSystem.java
+++ NavigationSystem.java
@@ -185,12 +185,8 @@ public class NavigationSystem {
   public boolean setNextMove() {
     if(has_destination && !moveControl.isActive()) {
       switch(mode) {
-        case NavigationMode.A_STAR:
-          return a_star();
         case NavigationMode.BUG:
           return bug();
-        case NavigationMode.FLOCK:
-          return flock();
       }
       System.out.println("Warning: fell through NavigationSystem.setNextMove (bad NavMode)");
     }
@@ -316,32 +312,4 @@ public class NavigationSystem {
     return false;
   }
 
-  /**
-   * The robot tries to flock with other robots around it, mimicing their movement
-   * Note: not implemented
-   * @return if the moveControl was set
-   */
-  protected boolean flock() {
-    //TODO: Implement this
-    //prevents a loop
-    System.out.println("WARNING: Tried to use the unimplemented method flock in NavSys.java");
-    has_destination = false;
-    return false;
-  }
-
-  /**
-   * The A* algorithm, I don't know how well this will work until later in the game
-   * as newly created robots can get very little information about the map until they've moved
-   * around for a while. It might be worth switching too after the robot has been around
-   * for a few hundred turns however
-   * Note: not implemented
-   * @return if the moveController was set
-   */
-  protected boolean a_star() {
-    //TODO: Implement this
-    //prevents a loop
-    System.out.println("WARNING: Tried to use the unimplemented method a_star in NavSys.java");
-    has_destination = false;
-    return false;
-  }
 }
--- SensorNavigationSystem.java
+++ SensorNavigationSystem.java
@@ -15,6 +15,74 @@ public class SensorNavigationSystem extends NavigationSystem {
     this.sensorSys = sensorSys;
 
   }
+
+  /**
+   * setNextMove is called to choose and execute the next for a bot.
+   * @return if the moveController was 'set' (given a command)
+   */
+  @Override
+  public boolean setNextMove() {
+    if(has_destination && !moveControl.isActive()) {
+      switch(mode) {
+        case NavigationMode.A_STAR:
+          return a_star();
+        case NavigationMode.BUG:
+          return bug();
+        case NavigationMode.FLOCK:
+          return flock();
+      }
+      System.out.println("Warning: fell through NavigationSystem.setNextMove (bad NavMode)");
+    }
+    //if the moveController is active or we don't have a destination return false
+    System.out.println("WARNING: Bad call to NavSys.setNextMove");
+    return false;
+  }
+
+
+
+  /**
+   * The robot tries to flock with other robots around it, mimicing their movement
+   * Note: not implemented
+   * @return if the moveControl was set
+   */
+  protected boolean flock() {
+    //TODO: Implement this
+    //prevents a loop
+    System.out.println("WARNING: Tried to use the unimplemented method flock in NavSys.java");
+    has_destination = false;
+    return false;
+  }
+
+  /**
+   * The robot tries to follow the leader bot
+   * @param bot the robot that this bot should follow
+   * @return if the moveControl was set
+   */
+  protected boolean followLeader(Robot bot) {
+    //TODO: Implement this
+    //prevents a loop
+    System.out.println("WARNING: Tried to use the unimplemented method followLeader in NavSys.java");
+    has_destination = false;
+    return false;
+  }
+
+
+  /**
+   * The A* algorithm, I don't know how well this will work until later in the game
+   * as newly created robots can get very little information about the map until they've moved
+   * around for a while. It might be worth switching too after the robot has been around
+   * for a few hundred turns however. It would also be great for getting to location within sensor
+   * range
+   * Note: not implemented
+   * @return if the moveController was set
+   */
+  protected boolean a_star() {
+    //TODO: Implement this
+    //prevents a loop
+    System.out.println("WARNING: Tried to use the unimplemented method a_star in NavSys.java");
+    has_destination = false;
+    return false;
+  }
   
 
 
